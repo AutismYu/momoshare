1212add396b014fb06ee5851d9ee8e338f52b667
AutismYu <72010336+AutismYu@users.noreply.github.com>
2024-01-23 18:45:04 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=33535791&pid=efc98ecd56e2eee06b1c6c2d7a578d0c&tid=49ad06389b710a6b9d2b52d5d6ba12c9",
+    "https://www.maimemo.com/share/page?uid=33535791&pid=305d53379f930e92ad9b41e2e3814310&tid=741bf1bf3b4ae614df8daa2752c17ad6",
 
 
   ]
